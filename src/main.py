0c715bcf1acc8c8214716bb8cf6b09494e654561
kokomi simp :3 <beol0127@gmail.com>
2022-02-17 14:01:21 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import os
+import time
+import sys
+import getpass
+import cursor
+import getkey
+from replit import db
+
+key_up = "\x1b[A"
+key_down = "\x1b[B"
+key_right = "\x1b[C"
+key_left = "\x1b[D"
