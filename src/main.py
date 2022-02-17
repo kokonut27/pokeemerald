57c514cb1a8ff07a6b815b02aad870ac87465808
kokomi simp :3 <beol0127@gmail.com>
2022-02-17 14:48:10 +0000

    chore: setup base code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 import pygame
+import os
 from replit import db
 
 key_up = "\x1b[A"
 key_down = "\x1b[B"
 key_right = "\x1b[C"
 key_left = "\x1b[D"
+
+def pokeemerald():
+  pygame.init()
+  logo = pygame.image.load("media/logo.jpeg")
+  pygame.display.set_icon(logo)
+  pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+  running = True
+
+  while running:
+    pygame.display.update()
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+        exit()
+
+if __name__ == "__main__":
+  os.system("clear")
+  pokeemerald()
